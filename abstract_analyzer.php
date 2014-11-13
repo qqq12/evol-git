6581747a428bef4aa631e279114bfc4f62a293cc
KalinkinaMaria <mashyfycya@gmail.com>
2014-11-13 15:44:52 +0300

    Branch change 2

--- abstract_analyzer.php
+++ abstract_analyzer.php
@@ -30,6 +30,10 @@
  * @package questions
  */
 
+ /**
+* Change to branch (this branch)
+*/
+ 
 defined('MOODLE_INTERNAL') || die();
 
 require_once($CFG->dirroot.'/question/type/correctwriting/string_pair.php');
